3c89b2b08c87f1bc1e774cc67e391ce68137fb14
myyusuf <myyusuf1911@gmail.com>
2017-12-09 16:00:19 +0700

    Proyek

--- src/components/workspace/Workspace.js
+++ src/components/workspace/Workspace.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import Layout, { Header, Footer, Content, Sider } from 'antd/lib/layout';
+import Menu from 'antd/lib/menu';
+import Icon from 'antd/lib/icon';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 const Workspace = () => {
   return (
-    <Layout>
-      <Header>Header</Header>
+    <Layout style={{ height: '100%' }}>
+      <Header
+        style={{
+          backgroundColor: '#F4F4F4',
+          color: '#1faeff',
+          fontSize: '15px' }}
+      >
+        DPE Dashboard
+      </Header>
       <Layout>
-        <Sider>Sider</Sider>
+        <Sider>
+          <Menu
+            onClick={this.handleClick}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+          >
+            <SubMenu key="sub1" title={<span><Icon type="solution" /><span>Proyek</span></span>}>
+              <Menu.Item key="1">Daftar Proyek</Menu.Item>
+              <Menu.Item key="2">Progress Proyek</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="setting" /><span>Settings</span></span>}>
+              <Menu.Item key="3">User</Menu.Item>
+              <Menu.Item key="4">Role</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
         <Content>Content</Content>
       </Layout>
-      <Footer>Footer</Footer>
+      <Footer>&copy; WIKA DPE 2018</Footer>
     </Layout>
   );
 };
